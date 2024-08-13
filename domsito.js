67870d3253ea73f1ec12f4f2c7c0dce278b903f3
paola50308 <hansumemiku@gmail.com>
2024-08-12 22:16:41 -0600

    Add files via upload

---
+++ domsito.js
@@ -0,0 +1,9 @@
+
+function calcular(){
+
+let nombre=document.getElementById("name").value;
+let edad=parseInt(document.getElementById("edad1").value);
+let aumento;
+aumento=edad+10;
+alert("Su edad aumentada es:"+aumento);
+}
